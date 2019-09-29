33fbfc7b5cada24a96f69310ba9110f53543e66c
youvovk <wer3wollf@gmail.com>
2019-09-29 16:38:39 +0300

    Updates

---
+++ precache-manifest.cffd241b333b3dad50bd7c7ebc064587.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a140e7066c097e13625f59a9cb14f90",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "688c39c04c69ddfb2159",
+    "url": "/react_tabs-with-router/static/css/main.56f90bea.chunk.css"
+  },
+  {
+    "revision": "f735c0c9f371916ca6ba",
+    "url": "/react_tabs-with-router/static/js/2.625088f1.chunk.js"
+  },
+  {
+    "revision": "688c39c04c69ddfb2159",
+    "url": "/react_tabs-with-router/static/js/main.86725d1f.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
